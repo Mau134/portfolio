453f392bc40e2440b19ac23b9e28887413ddb9d4
Maurice <molande.mau@gmail.com>
2024-12-12 06:42:50 +0200

    portfolio section

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import Navbar from "./components/NavBar/navbar";
 import Intro from "./components/Intro/intro";
 import Skills from "./components/Skills/skills";
+import Works from "./components/Works/works";
 
 function App() {
   return (
     <div className="App">
-      {/* Navbar for navigation */}
+      
       <Navbar />
-
-      {/* Intro section with an ID for smooth scrolling */}
       <section id="home">
         <Intro />
       </section>
 
-      {/* Skills section with an ID for smooth scrolling */}
       <section id="about">
         <Skills />
+      </section>
+      
+      <section id="portfolio">
+        <Works />
       </section>
     </div>
   );
--- src/components/Skills/skills.js
+++ src/components/Skills/skills.js
@@ -11,29 +11,41 @@ const skills = () => {
         What I do
       </span>
       <span className='skillDesc'>
-        I am fluent in PHP, HTML, CSS/SCSS, JavaScript and C++ programming languages. When it comes to database technologies, I am skilled in MYSQL. I am also familiar with frameworks such as Reactjs. I have hands on experience with the Internet of Things (IoT) technologies, having worked with technologies such as Arduino, ESP82 and RFID reader in my final year. I am also familiar in working with collaborative environments such as github for projects.
+        I am fluent in PHP, HTML, CSS/SCSS, JavaScript and C++ programming languages. When it comes to database technologies, I am skilled in MYSQL. I am also familiar with frameworks such as Reactjs.
+         I have hands on experience with the Internet of Things (IoT) technologies
+         having worked in a few projects in IoT. 
+         I am also familiar in working with collaborative environments such as github for projects.
       </span>
       <div className='skillBars'>
         <div className='skillBar'>
           <img src={UIDesign} alt='UIDesign' className='skillBarImg' />
           <div className='skillBarText'>
             <h2>UI/UX Design</h2>
-            <p>Reprehenderit id elit aliquip aliquip dolore esse. Aute eu in labore labore dolor qui fugiat. Excepteur culpa adipisicing excepteur culpa adipisicing reprehenderit adipisicing quis eu incididunt consectetur labore. Deserunt ea ut nostrud est occaecat. Culpa sint commodo nostrud aliqua ullamco ea dolore proident elit non eu.</p>
-          </div>
+            <p>
+              Proficient in creating user-centered designs that ensure intuitive and engaging experiences.
+              Skilled in wireframing, prototyping, and user testing to deliver visually appealing interfaces
+              that enhance usability and meet client objectives.
+            </p>          </div>
         </div>
         <div className='skillBar'>
           <img src={WebDesign} alt='Web Design' className='skillBarImg' />
           <div className='skillBarText'>
             <h2>Web Design</h2>
-            <p>Labore non dolor anim aliquip tempor culpa incididunt eu et dolore laborum officia. Incididunt ex excepteur qui incididunt dolor. Qui elit ullamco consequat sit proident mollit proident laboris labore consectetur enim. Qui amet quis eu pariatur aliquip aliquip ut eu ipsum. Id exercitation nisi cupidatat aliqua sint velit Lorem excepteur aliqua in. Consectetur officia tempor tempor esse tempor anim proident. Nulla et commodo quis aliqua mollit quis enim enim mollit.</p>
-          </div>
+            <p>
+              Experienced in crafting responsive and accessible websites using modern technologies like
+              HTML, CSS(SCSS), JavaScript, and frameworks like React. Expertise in ensuring cross-browser
+              compatibility, optimized performance, and seamless user experience.
+            </p>          </div>
         </div>
         <div className='skillBar'>
           <img src={AppDesign} alt='App Design' className='skillBarImg' />
           <div className='skillBarText'>
             <h2>Application Design</h2>
-            <p>Irure tempor laboris labore laboris dolor amet. Cillum irure deserunt incididunt duis incididunt non amet pariatur aliquip. Nulla anim commodo qui proident incididunt aliquip culpa ut. Fugiat laboris laborum in elit ipsum ipsum pariatur id sit. Aute do occaecat mollit cillum quis adipisicing. Aute mollit velit ipsum duis.</p>
-          </div>
+            <p>
+              Skilled in designing and developing dynamic web and mobile applications, focusing on functionality
+              and user engagement. Expertise in integrating APIs, payment systems, and implementing best
+              practices for scalable and maintainable code.
+            </p>          </div>
         </div>
       </div>
     </section>
